50c271f7c2f9de0569d96ec3e18f88f6c32a9eeb
Huseyin Uztetik <husuzttemu@gmail.com>
2018-12-31 19:58:16 +0300

    adding data service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../accounts.service';
 
 
 @Component({
@@ -9,17 +10,12 @@ import { LoggingService } from '../logging.service';
   providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
   // create an instance has to be injected. Type is not an optional
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+    private accountService: AccountService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
+    this.accountService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
 
      // console.log('A server status changed, new status: ' + accountStatus);
